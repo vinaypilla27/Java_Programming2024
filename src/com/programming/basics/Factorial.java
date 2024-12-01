99fc2e4651c8eef6c5dd0a097914417c9a880038
vinaypilla27 <vinaypilla27@gmail.com>
2024-12-01 20:28:38 +0530

    Comment added

--- src/com/programming/basics/Factorial.java
+++ src/com/programming/basics/Factorial.java
@@ -3,7 +3,7 @@ package com.programming.basics;
 public class Factorial {
 
 	public static void main(String[] args) {
-		
+		//THis is a program for factorial
 		System.out.println("This is a program for Factorial");
 
 	}
